878e58a028978718704cd114613f4d12f3d6f3b2
Yue Wu <272380923@qq.com>
2017-07-26 10:19:22 -0500

    111file

---
+++ 111/1.cs
@@ -0,0 +1,19 @@
+using System;
+namespace Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//I have been back I don't want to be merged
+			//I am in Dev Branch I don't want to be merged
+			//I have been back
+			//I am in Dev Branch
+			//I am in independent Dev
+			int a = 0;
+			a=1;
+			Console.Write("Hi");
+			Console.Readkey();
+		}
+	}
+}
